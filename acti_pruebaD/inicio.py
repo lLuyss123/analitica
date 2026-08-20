794a3e6a8ccf75bdd556b68d17863e2e649864bb
luis <lmejiaidep@gmail.com>
2026-08-20 10:46:49 -0500

    cambios

--- acti_pruebaD/inicio.py
+++ acti_pruebaD/inicio.py
@@ -11,11 +11,33 @@ print(df.dtypes)
 
 print("\n========== VALORES NULOS ==========")
 print(df.isnull().sum())
-print(df[df.isnull().any(axis=1)])
+nulos=df[df.isnull().any(axis=1)]
+nulos.to_csv('nulos.csv',index=False)
 
 print("\n========== DUPLICADOS ==========")
 print(df.duplicated().sum())
+duplicados=df[df.duplicated(subset="appid", keep=False)].sort_values("appid")
+duplicados.to_csv('duplicados.csv',index=False)
 print(df[df.duplicated()])
 
 print("\n========== PRIMERAS FILAS ==========")
 print(df.head())
+
+print(df[df["name"].isna()])
+
+
+#LIMPIEZA
+
+df = df.drop_duplicates()
+df.to_csv('newCSV.csv',index=False)
+
+
+
+
+# otra vez ETL
+
+print(df[["name"]].value_counts())
+
+print(df[df["name"]=='Bounce'])
+
+print(df[["developer"]].value_counts())
